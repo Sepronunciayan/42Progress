7989e030ecbe448af0a3a1260f3a2983aa8235dc
Sepronunciayan <sepronunciayan@gmail.com>
2025-04-24 13:35:44 +0200

    Update ft_range.c

--- exam_rank_02/Level 3/ft_range/ft_range.c
+++ exam_rank_02/Level 3/ft_range/ft_range.c
@@ -1,25 +1,37 @@
 #include <stdlib.h>
-#include <stdio.h>
 
 int	*ft_range(int start, int end)
 {
 	int *range;
 	int size;
 	int i;
-	
-	size = end - start + 1;
-	if (start > end)
+
+	if (start <= end)
+		size = end - start + 1;
+	else
 		size = start - end + 1;
 
 	range = malloc(size * sizeof(int));
 	if (!range)
-		return (NULL);
-
-	for (i = 0; start <= end && i < size; i++, start++)
-		range[i] = start;
-	
-	for (i = 0; start >= end && i < size; i++, start--)
-		range[i] = start;
-
+		return (0);
+	i = 0;
+	if (start <= end)
+	{
+		while (start <= end)
+		{
+			range[i] = start;
+			i++;
+			start++;
+		}
+	}
+	else
+	{
+		while (start >= end)
+		{
+			range[i] = start;
+			i++;
+			start--;
+		}
+	}
 	return (range);
 }
