b2e6e9f92e0957651991afae59cd28b4ac6c69ff
Sepronunciayan <sepronunciayan@gmail.com>
2025-06-11 19:39:41 +0200

    Update print_bits.c

--- exam_rank_02/Level 2/print_bits/print_bits.c
+++ exam_rank_02/Level 2/print_bits/print_bits.c
@@ -1,16 +1,17 @@
 #include <unistd.h>
 
-void    print_bits(unsigned char octet)
+void	print_bits(unsigned char octet)
 {
-    int i;
-    
-    i = 7;
-    while (i >= 0)
-    {
-       if((octet >> i) & 1)
-           write(1, "1", 1);
-       else 
-           write(1, "0", 1);
-       i--;
-    }
+	int	bit;
+
+	bit = 128;
+	while (bit > 0)
+	{
+		if (octet & bit)
+			write(1, "1", 1);
+		else
+			write(1, "0", 1);
+		bit = bit / 2;
+	}
+	write(1, "\n", 1);
 }
